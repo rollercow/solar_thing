4394c27cbeebd1e19cb380371ef2ca31f624756d
Chris Jones <c.jones@swansea.ac.uk>
2022-04-28 17:30:49 +0100

    make the datetime the index

--- import-excel.py
+++ import-excel.py
@@ -33,6 +33,9 @@ df2 = df.rename(
 # convert our timestamp to a datetime
 df2["updatetime"] = pd.to_datetime(df2["updatetime"])
 
+# make our new data time the index
+df2.set_index('updatetime', inplace=True)
+
 # append it to our DB
 df2.to_sql("solargeneration", engine, if_exists="append", index=False)
 
