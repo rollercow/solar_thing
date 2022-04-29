2404aca100763be5f46985055ab87c54ba468359
Chris Jones <c.jones@swansea.ac.uk>
2022-04-29 17:17:13 +0100

    complete the script to process data from DB into a meter like format we can use later

--- halfhourize.py
+++ halfhourize.py
@@ -1,27 +1,23 @@
 from datetime import timedelta
 import pandas as pd
+from sqlalchemy import create_engine
 
-df2 = pd.read_excel("foo.xls", skiprows=4)
+# connect to our DB
+engine = create_engine("postgresql://@/chris")
 
-# find the unique days in our blob of data
-df2.index.to_period("D").unique()
-
-# append a zero to the end of the day
-next = df2.index[-1] + timedelta(minutes=5)
-df = pd.DataFrame(columns=["wattac"])
-df.loc[next] = [0]
-pd.concat([df2, df])
-
-# interpolate to improve the resampleing
-foo = pd.date_range("2022-02-24", periods=1440, freq="1T")
-bar = pd.concat([df2, pd.DatetimeIndex.to_frame(foo)])
-bar.sort_index()["wattac"].interpolate(method="pchip", limit_area="inside").resample(
-    "1h"
-).mean()
+# Pull back the imported excel data from the table we aggregate it in
+df = pd.read_sql(
+    "select distinct updatetime, wattac, dayyeild from solargeneration",
+    engine,
+    index_col="updatetime",
+).sort_index()
 
 
 def makeHalfHour(DaysData):
-    # append a zero to the end of the day, 5min after the end of our data
+    """Builds nicely interpolated half hour windows for solar generation in kW/h
+    from the 5min data the SolaX inverter reporting gives you"""
+    # append a zero read to the end of the day, 5min after the end of our data,
+    # since the inverter has gone to sleep now
     nextRowTimestamp = DaysData.index[-1] + timedelta(minutes=5)
     # make ourselves a new frame to append to the end of our data
     newFrame = pd.DataFrame(columns=["wattac"])
@@ -49,9 +45,24 @@ halfHourGen = []
 # give me a day delta
 aDay = timedelta(days=1)
 # find the unique days in our blob of data
-for day in df2.index.to_period("D").unique():
+for day in df.index.to_period("D").unique():
     # make a timestamp
     thisDay = day.to_timestamp()
-    # use it to slice our data blob and generate our half hourly return
-    halfHourGen.append(makeHalfHour(df2.loc[thisDay : thisDay + aDay]))  # noqa: E203
+    # use it to slice our data blob
+    daySlice = df.loc[thisDay : thisDay + aDay]  # noqa: E203
+    # generate our half hourly return
+    meterLikeData = makeHalfHour(daySlice)
+    realYeild = daySlice["dayyeild"].max()
+    estYeild = meterLikeData.sum()
+    # work out the ratio between calculated and real yeild
+    dailyYeildRatio = (realYeild * 1000) / estYeild
+    # print our ratio
+    # print(str(day) + " - " + str(dailyYeildRatio))
+    # scale our days data based on the ratio
+    scaledMeterLikeData = meterLikeData * dailyYeildRatio
+    # feed that into the list to build a results frame
+    halfHourGen.append(scaledMeterLikeData.dropna())
+
 solarMeterEquiv = pd.concat(halfHourGen)
+# write it out to the DB
+solarMeterEquiv.to_sql("metersolargen", engine, if_exists="replace")
